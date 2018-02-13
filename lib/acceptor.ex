e1535b5f461ec5832999d22533f3428445d7aaea
Matthew Brookes <mbrookes1304@gmail.com>
2018-02-13 20:48:47 +0000

    Implement acceptor

---
+++ lib/acceptor.ex
@@ -0,0 +1,28 @@
+# Matthew Brookes (mb5715) and Abhinav Mishra (am8315)
+
+defmodule Acceptor do
+  def start do
+    next -1, MapSet.new
+  end
+
+  defp next ballot_number, accepted do
+    receive do
+      { :p1a, leader, b } ->
+        if b > ballot_number do
+          send leader, { :p1b, self(), b, accepted }
+        else
+          send leader, { :p1b, self(), ballot_number, accepted }
+        end
+        next ballot_number, accepted
+
+      { :p2a, leader, { b, _, _ } = pvalue } ->
+        accepted =
+          case b == ballot_number do
+            true -> MapSet.put(accepted, pvalue)
+            false -> accepted
+          end
+        send leader, { :p2b, self(), ballot_number }
+        next ballot_number, accepted
+    end
+  end
+end
