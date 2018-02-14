dec7bf2808be58ff7016f7c67cd7160139afed12
Matthew Brookes <mbrookes1304@gmail.com>
2018-02-14 19:17:11 +0000

    Implement Commander

---
+++ lib/commander.ex
@@ -0,0 +1,26 @@
+# Matthew Brookes (mb5715) and Abhinav Mishra (am8315)
+
+defmodule Commander do
+
+  def start leader, acceptors, replicas, pvalue do
+    for acceptor <- acceptors, do: send acceptor, { :p2a, self(), pvalue }
+    next leader, acceptors, replicas, pvalue, acceptors
+  end
+
+  defp next leader, acceptors, replicas, { ballot_number, s, c }, wait_for do
+    receive do
+      { :p2b, a, b } ->
+        if b == ballot_number do
+          wait_for = MapSet.delete wait_for, a
+          if MapSet.size wait_for < MapSet.size acceptors / 2 do
+            for replica <- replicas, do: send replica, { :decision, s, c }
+            Process.exit self(), :kill
+          end
+        else
+          send leader, { :preempted, b }
+          Process.exit self(), :kill
+        end
+    end
+  end
+
+end
